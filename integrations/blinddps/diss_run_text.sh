d40db7431fc616cbf6a59c9629ae7056548d5cc8
vishnutez <72103342+vishnutez@users.noreply.github.com>
2025-11-20 11:55:02 -0600

    Update diss_run_text.sh

--- integrations/blinddps/diss_run_text.sh
+++ integrations/blinddps/diss_run_text.sh
@@ -1,21 +1,3 @@
-#!/bin/bash
-#SBATCH --job-name=Blind-DPS         # Job name
-#SBATCH --mail-type=BEGIN,END,FAIL            # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --ntasks=8                      # Run on a 8 cpus (max)
-#SBATCH --gres=gpu:tesla:1              # Run on a single GPU (max)
-#SBATCH --partition=gpu-research                 # Select GPU Partition
-#SBATCH --qos=olympus-research-gpu          # Specify GPU queue
-#SBATCH --time=72:00:00                 # Time limit hrs:min:sec current 5 min - 36 hour max
-
-# use the sbatch command to submit your job to the cluster.
-# sbatch train.sh
-
-# select your singularity shell (currently cuda10.2-cudnn7-py36)
-singularity shell /mnt/lab_files/ECEN403-404/containers/cuda_10.2-cudnn7-py36.sif
-# source your virtual environmnet
-cd /mnt/shared-scratch/Narayanan_K/mahdi.farahbakhsh/DISS/integrations/blinddps
-source activate DPS_paper
-
 # Set PYTHONPATH directly
 export PYTHONPATH=blind-dps:../../../DISS:../../../DISS/third_party/AdaFace
 
